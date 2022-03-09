624663005f4d4c1bee9f901207132dcf23d84a3c
dojoprojectrepos <98948870+dojoprojectrepos@users.noreply.github.com>
2022-03-09 17:36:42 +0200

    Update MultiplyTask.java

--- src/main/java/test/parent/MultiplyTask.java
+++ src/main/java/test/parent/MultiplyTask.java
@@ -2,14 +2,14 @@ package test.parent;
 
 public class MultiplyTask {
     public static int multiply(int x, int y){
-//         boolean flag = true;
-//         while(flag){
+        boolean flag = true;
+        while(flag){
 //             System.out.println("pet");
        
 //         if(y==-67){
 //             x=67;
 //         }
-//     }
+    }
         return x * y;
     }
 }
